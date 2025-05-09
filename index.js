43031ea38acdc037d45b30288df90d5efbd795ff
zeroaltud <imaslaveofyoupapi@gmail.com>
2025-05-09 13:39:21 +0200

    mooi

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
-const express = require('express');
-const { createProxyMiddleware } = require('http-proxy-middleware');
-const app = express();
+const Proxy = require('http-mitm-proxy');
+const proxy = Proxy();
 
-// Proxy all /netflix/* requests
-app.use('/netflix', createProxyMiddleware({
-  target: 'https://www.netflix.com',
-  changeOrigin: true,
-  pathRewrite: {
-    '^/netflix': '', // remove /netflix from URL
-  },
-  onProxyRes(proxyRes, req, res) {
-    delete proxyRes.headers['content-security-policy'];
-    delete proxyRes.headers['x-frame-options'];
-  }
-}));
+// Optional logging
+proxy.onError((ctx, err) => {
+  console.error('Proxy error:', err);
+});
 
-app.listen(3000, () => {
-  console.log('Proxy running on http://localhost:3000');
+proxy.onRequest((ctx, callback) => {
+  ctx.onResponseHeaders((ctx, callback) => {
+    // Strip headers that block iframe embedding
+    delete ctx.serverToProxyResponse.headers['x-frame-options'];
+    delete ctx.serverToProxyResponse.headers['content-security-policy'];
+    callback(null, true);
+  });
+
+  return callback();
+});
+
+// Start proxy on port 8000
+proxy.listen({ port: 8000 }, () => {
+  console.log('Proxy listening on port 8000');
 });
